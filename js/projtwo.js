d7653d7358f42b45bc909f481db664a693b8e12f
omarhatab99 <analove_egypt@yahoo.com>
2021-07-05 17:35:29 +0200

    first commit

--- js/projtwo.js
+++ js/projtwo.js
@@ -1,13 +1,11 @@
 //--------counter input -------
-var myShowCounter = document.getElementById('showCounter');
-
 let addBtn = document.querySelector('#add');
 let subBtn = document.querySelector('#sub');
 let qty = document.querySelector('#qtyBox');
 
-addBtn.addEventListener('click',()=>{
+addBtn.onclick=function(){
      qty.value = parseInt(qty.value) + 1;
-});
+}
 
 subBtn.addEventListener('click',()=>{
      if (qty.value <= 0) {
